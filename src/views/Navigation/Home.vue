9a9b03b14017c72bfc8251437366056d59e1f5d2
Eric Chen <1925264401@qq.com>
2023-03-16 18:05:56 +0800

    add components

--- src/views/Navigation/Home.vue
+++ src/views/Navigation/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex m-2">
+  <!-- Line 1 -->
+  <div class="out-flex-box">
     <!-- button -->
     <div class="flex flex-col flex-1">
       <div class="inline-flex-box">
@@ -77,7 +78,8 @@
       </div>
     </div>
   </div>
-  <div class="flex m-2">
+  <!-- Line 2 -->
+  <div class="out-flex-box">
     <!-- avatar -->
     <div class="flex flex-col flex-1">
       <div class="inline-flex-box">
@@ -134,7 +136,7 @@
         </div>
       </div>
     </div>
-    <!-- card -->
+    <!-- card1 -->
     <div class="flex flex-col flex-1">
       <div class="inline-flex-box">
         <div class="card w-64 glass">
@@ -149,6 +151,7 @@
         </div>
       </div>
     </div>
+    <!-- card2 -->
     <div class="flex flex-col flex-1 justify-center">
       <div class="card w-96 bg-base-100 shadow-xl image-full">
         <figure><img src="../../assets/images/bg2.png" alt="Shoes" /></figure>
@@ -162,6 +165,201 @@
       </div>
     </div>
   </div>
+  <!-- Line 3 -->
+  <div class="out-flex-box">
+    <!-- carousel -->
+    <div class="flex flex-col flex-1">
+      <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+          <img src="../../assets/images/wallhaven-1.jpg" class="w-full" />
+        </div> 
+        <div id="item2" class="carousel-item w-full">
+          <img src="../../assets/images/wallhaven-2.jpg" class="w-full" />
+        </div> 
+        <div id="item3" class="carousel-item w-full">
+          <img src="../../assets/images/wallhaven-3.jpg" class="w-full" />
+        </div> 
+        <div id="item4" class="carousel-item w-full">
+          <img src="../../assets/images/wallhaven-4.jpg" class="w-full" />
+        </div>
+      </div> 
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-xs">1</a> 
+        <a href="#item2" class="btn btn-xs">2</a> 
+        <a href="#item3" class="btn btn-xs">3</a> 
+        <a href="#item4" class="btn btn-xs">4</a>
+      </div>
+    </div>
+    <!-- chat bubble -->
+    <div class="flex flex-col flex-1 justify-center mx-10">
+      <div class="chat chat-start">
+        <div class="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+      </div>
+      <div class="chat chat-start">
+        <div class="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+      </div>
+      <div class="chat chat-end">
+        <div class="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+      </div>
+      <div class="chat chat-end">
+        <div class="chat-bubble chat-bubble-error">It's never happened before.</div>
+      </div>
+    </div>
+    <!-- Collapse -->
+    <div class="flex flex-col flex-1 justify-center">
+      <div class="text-center my-2 text-cyan-700 font-bold">点击我有惊喜呦！</div>
+      <div class="collapse rounded-lg">
+        <input type="checkbox" class="peer" /> 
+        <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Click me to show/hide content
+        </div>
+        <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Line 4 -->
+  <div class="out-flex-box">
+    <div class="flex flex-col felx-1"></div>
+    <div class="flex flex-col felx-1"></div>
+    <div class="flex flex-col felx-1"></div>
+  </div>
+  <!-- Line 5 -->
+  <div class="out-flex-box">
+    <!-- countDown -->
+    <div class="flex flex-col flex-1">
+      <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:15;"></span>
+          </span>
+          days
+        </div> 
+        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:10;"></span>
+          </span>
+          hours
+        </div> 
+        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:24;"></span>
+          </span>
+          min
+        </div> 
+        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:28;"></span>
+          </span>
+          sec
+        </div>
+      </div>
+    </div>
+    <!-- Progress -->
+    <div class="flex flex-1 justify-center items-center">
+      <div>
+        <progress class="progress progress-error w-80" value="0" max="100"></progress>
+        <progress class="progress progress-error w-80" value="10" max="100"></progress>
+        <progress class="progress progress-error w-80" value="40" max="100"></progress>
+        <progress class="progress progress-error w-80" value="70" max="100"></progress>
+        <progress class="progress progress-error w-80" value="100" max="100"></progress>
+      </div>
+    </div>
+    <!-- Stat -->
+    <div class="flex flex-col flex-1">
+      <div class="stats bg-primary text-primary-content">
+        <div class="stat">
+          <div class="stat-title">Account balance</div>
+          <div class="stat-value">$89,400</div>
+          <div class="stat-actions">
+            <button class="btn btn-sm btn-success">Add funds</button>
+          </div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Current balance</div>
+          <div class="stat-value">$89,400</div>
+          <div class="stat-actions">
+            <button class="btn btn-sm">Withdrawal</button> 
+            <button class="btn btn-sm">deposit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Line 6 -->
+  <div class="out-flex-box">
+    <!-- CheckBox / FileInput / Raido / Rage / Rating -->
+    <div class="flex flex-col flex-1 items-center">
+      <!-- CheckBox -->
+      <div class="form-control mb-3">
+        <label class="cursor-pointer label">
+          <input type="checkbox" checked class="checkbox checkbox-success" />
+          <input type="checkbox" checked class="checkbox checkbox-warning mx-3" />
+          <input type="checkbox" checked class="checkbox checkbox-error" />
+        </label>
+      </div>
+      <!-- FileInput -->
+      <div class="mb-3">
+        <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+      </div>
+      <!-- Raido -->
+      <div class="mb-3">
+        <input type="radio" name="radio-5" class="radio checked:bg-red-400" checked />
+        <input type="radio" name="radio-5" class="radio checked:bg-green-500 mx-3" />
+      </div>
+      <!-- Range -->
+      <div class="mb-3">
+        <input type="range" min="0" max="100" value="40" class="range range-primary w-72" />
+      </div>
+      <!-- Rating -->
+      <div class="mb-3">
+        <div class="rating gap-1">
+          <input type="radio" name="rating-3" class="mask mask-heart bg-red-400" />
+          <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" checked />
+          <input type="radio" name="rating-3" class="mask mask-heart bg-yellow-400" />
+          <input type="radio" name="rating-3" class="mask mask-heart bg-lime-400" />
+          <input type="radio" name="rating-3" class="mask mask-heart bg-green-400" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col flex-1">
+      <!-- Select -->
+      <div class="mt-3">
+        <select class="select select-secondary w-full max-w-xs">
+          <option disabled selected>Pick your favorite language</option>
+          <option>Java</option>
+          <option>Go</option>
+          <option>C</option>
+          <option>JavaScript</option>
+          <option>Python</option>
+        </select>
+      </div>
+      <!-- Input -->
+      <div class="mt-3">
+        <input type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs" />
+      </div>
+      <!-- Textarea -->
+      <div class="mt-3">
+        <textarea class="textarea textarea-secondary w-80" placeholder="Bio"></textarea>
+      </div>
+    </div>
+    <div class="flex flex-col flex-1">
+      <div class="mt-3">
+        <input type="checkbox" class="toggle toggle-success mx-3" checked />
+        <input type="checkbox" class="toggle toggle-warning mx-3" checked />
+        <input type="checkbox" class="toggle toggle-info mx-3" checked />
+        <input type="checkbox" class="toggle toggle-error mx-3" checked />
+      </div>
+      <div class="divider">OR</div>
+      <div class="btn-group mt-3">
+        <button class="btn btn-active">Button</button>
+        <button class="btn">Button</button>
+        <button class="btn">Button</button>
+      </div>
+      <div class="divider">OR</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -169,6 +367,9 @@
 </script>
 
 <style lang="scss" scoped>
+.out-flex-box {
+  @apply flex mx-2 my-2
+}
 .inline-flex-box {
   @apply flex justify-around m-2
 }
